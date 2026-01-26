080612b30665c2c151405b69670de868ca39310f
James-al7 <James_al7@outlook.com>
2026-01-26 18:12:33 +0000

    commit3

--- Homework1/ingest_taxi_data.py
+++ Homework1/ingest_taxi_data.py
@@ -16,7 +16,6 @@ import click
 @click.option('--pg-db', default='trip_data', help='PostgreSQL database name')
 @click.option('--year', default=2021, type=int, help='Year of the data')
 @click.option('--month', default=1, type=int, help='Month of the data')
-@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
 @click.option('--target-table', default='green_taxi_data', help='Target table name')
 
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
